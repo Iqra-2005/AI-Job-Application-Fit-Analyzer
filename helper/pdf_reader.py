a189bd52e394f1e3a75b827c53334188d1857681
Iqra Shaikh <157222455+Iqra-2005@users.noreply.github.com>
2025-12-28 15:30:52 +0530

    Delete helper directory

--- helper/pdf_reader.py
+++
@@ -1,8 +0,0 @@
-import pdfplumber
-
-def extract_text_from_pdf(pdf_file):
-    text = ""
-    with pdfplumber.open(pdf_file) as pdf:
-        for page in pdf.pages:
-            text += page.extract_text() + "\n"
-    return text
